8716bdcbff7089c75b4e96b9a5803797032c4bd7
Moritz Schilling <moritz.schilling@dfki.de>
2024-02-28 14:18:51 +0100

    Improved/fixed xdbi-fix-db tool
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- bin/xdbi-fix-db.py
+++ bin/xdbi-fix-db.py
@@ -4,27 +4,52 @@ import os
 import argparse
 import xdbi_py
 import xtypes_py
-
-
-backends = xdbi_py.get_available_backends()
+import copy
 
 
 def main():
+    backends = xdbi_py.get_available_backends()
+
+    config = {
+            "type": "MultiDbClient",
+            "config":
+                {
+                    "import_servers": [],
+                    "main_server":
+                        {
+                            "type": None,
+                            "address": None,
+                            "graph": None
+                        }
+                }
+            }
+
     parser = argparse.ArgumentParser(description='Tries to fix errors in the database. This is an expert tool! Use with caution.')
     # xdbi specific args
-    parser.add_argument('-b', '--db_backend', help="Database backend to be used", choices=backends, default=backends[0]) 
-    parser.add_argument('-a', '--db_address', help="The url/local path to the db", required=True) 
+    parser.add_argument('-b', '--db_backend', help="Database backend to be used", choices=backends, default=backends[0])
+    parser.add_argument('-a', '--db_address', help="The url/local path to the db", required=True)
+    parser.add_argument('-i', '--import_server', help="Specify an import server for additional lookup (format: <backend>,<address>,<graph>)", action="append", type=str)
     # tool specific args
-    parser.add_argument('-s','--src_graph', help="The name of the source graph", default="src_graph")
-    parser.add_argument('-d','--dst_graph', help="The name of the destination graph", default="dst_graph")
+    parser.add_argument('-s','--src_graph', help="The name of the source graph", default="src_graph", required=True)
+    parser.add_argument('-d','--dst_graph', help="The name of the destination graph", default="dst_graph", required=True)
     parser.add_argument('--max_depth_for_store', help="Maximum depth for storing the Xtypes in destination graph", default=1, type=int)
     parser.add_argument('--use_update', help="Use update instead of add when storing Xtypes in destination graph", action="store_true")
-    
+
     args = None
     try:
         args = parser.parse_args()
     except SystemExit:
         sys.exit(1)
+
+    config["config"]["main_server"]["type"] = args.db_backend
+    config["config"]["main_server"]["address"] = args.db_address
+    config["config"]["main_server"]["graph"] = args.dst_graph
+    import_servers = args.import_server
+    if import_servers:
+        for import_server in import_servers:
+            b,a,g = import_server.split(",", 2)
+            config["config"]["import_servers"].append( {"type": b.strip(), "address": a.strip(), "graph": g.strip() } )
+    config["config"]["import_servers"].append( { "name": "main", "type": args.db_backend, "address": args.db_address, "graph": args.src_graph } )
 
     if args.src_graph == args.dst_graph:
         print("Please choose a different destination graph")
@@ -34,12 +59,13 @@ def main():
     registry = xtypes_py.ProjectRegistry()   
 
     print(f"Accessing database at {args.db_address}")
-    src_dbi = xdbi_py.db_interface_from_config(registry, config={"type":args.db_backend, "address": args.db_address, "graph": args.src_graph}, read_only=True)
-    dst_dbi = xdbi_py.db_interface_from_config(registry, config={"type":args.db_backend, "address": args.db_address, "graph": args.dst_graph}, read_only=False)
+    dbi = xdbi_py.db_interface_from_config(registry, config=config, read_only=False)
 
     print(f"Looking up URIs in source graph ...")
+    src_registry = xtypes_py.ProjectRegistry()
+    src_dbi = xdbi_py.db_interface_from_config(src_registry, config=config["config"]["import_servers"][-1], read_only=True)
     uris = src_dbi.uris()
-    print(f"Found {len(uris)} uris.")
+    print(f"Found {len(uris)} uris in main database")
 
     to_be_stored = set()
     to_be_removed = set()
@@ -50,7 +76,7 @@ def main():
     for old_uri in uris:
         xtype = None
         try:
-            xtype = src_dbi.load(old_uri)
+            xtype = dbi.load(old_uri)
             new_uri = xtype.uri()
             # We also have to check if the uris of the references can be constructed (used in second pass)
             rels = xtype.get_relations()
@@ -60,9 +86,9 @@ def main():
                     for fact in facts:
                         fact.target.uri()
             loaded = True
-        except:
+        except Exception as error:
             # Here we found a broken entry
-            print(f"Marked broken entry {old_uri} to be removed (cannot be loaded correctly)")
+            print(f"Marked broken entry {old_uri} to be removed (cannot be loaded correctly): {error}")
             to_be_removed.add(old_uri)
             continue
         # Store resolved xtype
@@ -74,20 +100,30 @@ def main():
     for old_uri in to_be_stored:
         try:
             if args.use_update:
-                dst_dbi.update([xtypes[old_uri]], args.max_depth_for_store)
+                dbi.update([xtypes[old_uri]], args.max_depth_for_store)
             else:
-                dst_dbi.add([xtypes[old_uri]], args.max_depth_for_store)
+                dbi.add([xtypes[old_uri]], args.max_depth_for_store)
             stored += 1
         except Exception as error:
             print(f"WARNING: Could not store {old_uri}: {error}!")
 
+    print(f"Removing {len(to_be_removed)} xtypes in destination graph ...")
+    removed = 0
+    for old_uri in to_be_removed:
+        try:
+            dbi.remove(old_uri)
+            removed += 1
+        except Exception as error:
+            print(f"WARNING: Could not remove {old_uri}: {error}!")
+
     print("SUMMARY")
     print(f"Found {len(uris)} xtypes")
     print(f"Wanted to store {len(to_be_stored)} xtypes")
-    print(f"Stored {stored} xtypes")
-    print(f"Dropped {len(to_be_removed)} bogus/deprecated xtypes")
+    print(f"Wanted to remove {len(to_be_removed)} bogus/deprecated xtypes")
     if stored < len(to_be_stored):
         print(f"Stored only {stored} out of {len(to_be_stored)}.")
+    if removed < len(to_be_removed):
+        print(f"Removed only {removed} out of {len(to_be_removed)}.")
 
     script_name = sys.argv[0]
     print(f"{script_name} executed successfully")
